9397075dc21b5197f9e0290c5496787498ded591
kaoula <kaoulatouati@gmail.com>
2020-12-25 07:14:47 +0100

    upload file edits + test 2nd solution

---
+++ src/main/java/formalab/gestion/produits/exception/FileStorageException.java
@@ -0,0 +1,11 @@
+//package formalab.gestion.produits.exception;
+//
+//public class FileStorageException extends RuntimeException{
+//    public FileStorageException(String message) {
+//        super(message);
+//    }
+//
+//    public FileStorageException(String message, Throwable cause) {
+//        super(message, cause);
+//    }
+//}
